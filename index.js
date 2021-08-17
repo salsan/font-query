0863872a863a8b9e72a5143a15009c93c2e49bec
Salvatore Santagati <github@salsan.dev>
2021-08-17 15:27:16 +0200

    refactor: fix tab

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ module.exports = function fontQuery(fontName) {
     process.exit(9);
   }
 
-  switch(process.platform){
-    case 'win32' :
-       return fontWin(fontName);
-       break;
-    case 'linux' :
+  switch (process.platform) {
+    case 'win32':
+      return fontWin(fontName);
+      break;
+    case 'linux':
       return fontLinux(fontName);
       break;
     default:
