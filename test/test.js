2950708b4dc975bc5f724579cb4a98aae95813ba
Salvatore Santagati <github@salsan.dev>
2021-11-12 16:32:21 +0100

    chore: added support to mocha and eslint

---
+++ test/test.js
@@ -0,0 +1,15 @@
+const assert = require('assert')
+
+const fontQuery = require('../index.js')
+
+describe('Query', function () {
+  it('bal', function () {
+    assert.ok(fontQuery('bal'))
+  })
+  it('b', function () {
+    assert.ok(fontQuery('b'))
+  })
+  it('--v', function () {
+    assert.ok(fontQuery('v'))
+  })
+})
