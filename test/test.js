c3ca5071f6d985569c3a736843702b40f0afb4bb
salsan <me@salsan.dev>
2024-04-05 12:04:17 +0200

    chore: improved test

--- test/test.js
+++ test/test.js
@@ -2,26 +2,16 @@ const assert = require('assert')
 
 const { fontQuery, fontQueryAsync } = require('../index.js')
 
-describe('fontQueryAsync', function () {
-  it('bal', function () {
-    assert.ok(fontQueryAsync('bal'))
-  })
-  it('b', function () {
-    assert.ok(fontQueryAsync('b'))
-  })
-  it('--v', function () {
-    assert.ok(fontQueryAsync('--v'))
+describe('fontQueryAsync()', function () {
+  it('All fonts', async function () {
+    const fonts = await fontQueryAsync(' ')
+    assert.ok(fonts.length > 0)
   })
 })
 
-describe('fontQuery', function () {
-  it('bal', function () {
-    assert.ok(fontQuery('bal'))
-  })
-  it('b', function () {
-    assert.ok(fontQuery('b'))
-  })
-  it('--v', function () {
-    assert.ok(fontQuery('--v'))
+describe('fontQuery()', function () {
+  it('All fonts', function () {
+    const fonts = fontQuery(' ')
+    assert.ok(fonts.length > 0)
   })
 })
